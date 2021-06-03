e91fe2453a1c8d7aa013c20cee6d07ff90aa8cb0
Duong Dinh Cuong <cuong3ihut@gmail.com>
2021-06-03 14:28:15 +0200

    Update const.js

--- const.js
+++ const.js
@@ -4,11 +4,13 @@ const CRESET = '\x1b[0m'
 const OPT_COMMANDS = [
     {
         name: "list", desc: "list all stages of this pipeline", options: [
-            { name: "help", desc: "show a help for the list command" }
+            { name: "help", desc: "show a help for the list command" },
+            { name: "file", desc: "specific a file input for pipeline" }
         ]
     }, {
         name: "create", desc: "create docker-compose.yml for a selected stage", options: [
-            { name: "project", desc: "specify a project name for volume caching" }
+            { name: "project", desc: "specify a project name for volume caching" },
+            { name: "file", desc: "specific a file input for pipeline" }
         ]
     }
 ]
